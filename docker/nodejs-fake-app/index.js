8ff9dff6ff28f048846f461f0b670802aa8852c2
Roberto Andradas <randradas@vmware.com>
2021-11-25 18:36:46 +0100

    Remove extra semicolon to fix linting

--- docker/nodejs-fake-app/index.js
+++ docker/nodejs-fake-app/index.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-});
+})
 
 const server = app.listen(3000, '0.0.0.0', function () {
   const host = server.address().address
   const port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
-});
+})
