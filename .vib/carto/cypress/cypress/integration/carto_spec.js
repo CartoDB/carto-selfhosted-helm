2bb28f2bf1b1ba0ab6733e637afdee62279102c9
FraPazGal <fdepaz@vmware.com>
2022-03-21 13:52:37 +0100

    Add cypress tests
    Signed-off-by: FraPazGal <fdepaz@vmware.com>

---
+++ .vib/carto/cypress/cypress/integration/carto_spec.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+
+it('Login page is shown', () => {
+  cy.visit('https://carto.vmw/');
+  cy.get('img.logo').should('have.attr', 'src').should('include','carto-logo-negative.svg');
+});
